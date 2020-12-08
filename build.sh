4c88bbab05ae1a20e6bb361b697ea6adcec10529
Kenji Rikitake <kenji.rikitake@acm.org>
2020-12-08 03:19:45 +0000

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+c++ -std=c++17 -o ParSTLTests ParSTLTests.cpp
+c++ -std=c++17 -o FileSizes FileSizes.cpp
+c++ -std=c++17 -o WordCount WordCount.cpp
+c++ -std=c++17 -o FileWordCount FileWordCount.cpp
